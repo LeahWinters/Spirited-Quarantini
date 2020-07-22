ecb2f500ba32c7b6474904637a2b5387e46f7254
Leah Winters <leah.winters24@gmail.com>
2020-07-22 16:33:47 -0600

    Add basic test so file is not empty

---
+++ src/Login/Login.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Login from './Login.tsx';
+import '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+
+describe('Login', () => {
+  it("should be true", () => {
+    expect(true).toEqual(true);
+  });
+})
