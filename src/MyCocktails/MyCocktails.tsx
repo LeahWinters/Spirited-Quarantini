eb9f0fa5d67cec2d9c05fad2049bcb72ff998784
Melissa VanKempen <melissa.vankempen@gmail.com>
2020-07-22 14:53:33 -0600

    :crystal_ball: apicreation

--- src/MyCocktails/MyCocktails.tsx
+++ src/MyCocktails/MyCocktails.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { getAllCocktails } from '../apiCalls';
+import './MyCocktails.scss';
+
 
 interface MyCocktailsProps {
 }
 
 const MyCocktails: React.SFC<MyCocktailsProps> = () => {
+	const [allCocktails, setAllCocktails] = useState({})
+
+	const displayingAllCocktails = async () => {
+		try {
+			const data: any = await getAllCocktails();
+			setAllCocktails(data)
+		} catch (error) {
+
+		}
+	}
 
 	return (
 		<div>MY COCKTAILS PAGE</div>
