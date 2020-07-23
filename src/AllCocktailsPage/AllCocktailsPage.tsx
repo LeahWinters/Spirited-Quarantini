300dec476abfad448047d35ddf767ec319baf04d
Leah Winters <leah.winters24@gmail.com>
2020-07-23 14:40:56 -0600

    :crystal_ball: Add component for cocktail card

--- src/AllCocktailsPage/AllCocktailsPage.tsx
+++ src/AllCocktailsPage/AllCocktailsPage.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { getAllCocktails } from "../apiCalls";
+import CocktailCard from '../CocktailCard/CocktailCard';
 
 export interface AllCocktailsDetails {
   drinks: [
