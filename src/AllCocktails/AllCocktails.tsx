3a6a9ceb44a3e8c0696e99fdf60fe5616a90391d
Melissa VanKempen <melissa.vankempen@gmail.com>
2020-07-23 11:27:23 -0600

    :tada: initial commit

--- src/AllCocktails/AllCocktails.tsx
+++ src/AllCocktails/AllCocktails.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 
 interface AllCocktailsProps {
+	// 	{
+// 		"strDrink": "'57 Chevy with a White License Plate",
+// 		"strDrinkThumb": "https://www.thecocktaildb.com/images/media/drink/qyyvtu1468878544.jpg",
+// 		"idDrink": "14029"
+// }
 }
 
 const AllCocktails = () => {
