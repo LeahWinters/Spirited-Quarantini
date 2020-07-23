7d6b6be33b4fd9abc0c99f4da10d73ad56bc39e4
Alex Sodemann <asodemann18@gmail.com>
2020-07-23 17:02:50 -0600

    Fix error with using mockResolvedValue

--- src/Dashboard/Dashboard.test.tsx
+++ src/Dashboard/Dashboard.test.tsx
@@ -4,18 +4,18 @@ import "@testing-library/jest-dom";
 import { render, waitFor } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import { getRandomCocktail } from "../apiCalls";
-import { RandomCocktail } from '../Definitions/RandomCocktail'
+import { mocked } from "ts-jest/utils";
 jest.mock("../apiCalls");
 
 describe("Dashboard", () => {
-  getRandomCocktail.mockResolvedValue<RandomCocktail>({
+  mocked(getRandomCocktail).mockImplementation(() =>
+  Promise.resolve({
     idDrink: "11007",
     strDrink: "Margarita",
-    strInstructions:
-      "Rub the rim of the glass with the lime slice to make the salt stick to it. Take care to moisten only the outer rim and sprinkle the salt on it. The salt should present to the lips of the imbiber and never mix into the cocktail. Shake the other ingredients with ice, then carefully pour into the glass.",
-    strDrinkThumb:
-      "https://www.thecocktaildb.com/images/media/drink/5noda61589575158.jpg",
-  });
+    strInstructions: "Rub the rim of the glass with the lime slice to make the salt stick to it. Take care to moisten only the outer rim and sprinkle the salt on it. The salt should present to the lips of the imbiber and never mix into the cocktail. Shake the other ingredients with ice, then carefully pour into the glass.",
+    strDrinkThumb: "https://www.thecocktaildb.com/images/media/drink/5noda61589575158.jpg",
+  })
+);
 
   it("should show a welcome message", async () => {
     const { getByText } = render(
@@ -47,6 +47,11 @@ describe("Dashboard", () => {
     );
 
     const drinkName = await waitFor(() => getByText('Margarita'))
+    const drinkImg = await waitFor(() => getByAltText('Margarita'))
+    const button = await waitFor(() => getByRole('button', {name: 'More details'}))
+    
     expect(drinkName).toBeInTheDocument();
+    expect(drinkImg).toBeInTheDocument();
+    expect(button).toBeInTheDocument();
   });
 });
