66703163750af993b742ecccaedfad504dbe04ed
Melissa VanKempen <melissa.vankempen@gmail.com>
2020-07-23 11:43:41 -0600

    :crystal_ball: created state for allCocktails

--- src/AllCocktails/AllCocktails.tsx
+++ src/AllCocktails/AllCocktails.tsx
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { getAllCocktails } from '../AllCocktails/AllCocktails'
 
-interface AllCocktailsProps {
-	// 	{
+
+export interface AllCocktails {
+  strDrink: string;
+  strDrinkThumb: string;
+  idDrink: string;
+}
+
+const AllCocktails: React.SFC = () => {
+  const [allCocktails, setAllCocktails] = useState<AllCocktails>({
+    strDrink: "",
+    strDrinkThumb: "",
+    idDrink: "",
+	});
+	const [ error, setError ]= useState<string>('')
+
+  return <div>ALL COCKTAILS PAGE</div>;
+};
+
+export default AllCocktails;
+
+// "drinks": [
+// 	{
 // 		"strDrink": "'57 Chevy with a White License Plate",
 // 		"strDrinkThumb": "https://www.thecocktaildb.com/images/media/drink/qyyvtu1468878544.jpg",
 // 		"idDrink": "14029"
 // }
-}
-
-const AllCocktails = () => {
-
-	return (
-		<div>ALL COCKTAILS PAGE</div>
-	)
-}
-
-export default AllCocktails;
