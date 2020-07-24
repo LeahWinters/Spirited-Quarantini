6b44e2b86b7d4dd70e680c562c5fd47504b0faf5
Alex Sodemann <asodemann18@gmail.com>
2020-07-24 17:44:23 -0600

    Add error handling to getAllCocktails apiCall but it's breaking somewhere

--- src/apiCalls.tsx
+++ src/apiCalls.tsx
@@ -3,15 +3,28 @@ import { AllCocktailsDetails } from "./AllCocktailsPage/AllCocktailsPage";
 
 const rootUrl = "https://www.thecocktaildb.com/api/json/v1/1";
 
+export interface ResponseInfo {
+  type: string,
+  url: string,
+  redirected: boolean,
+  status: number,
+  ok: boolean
+}
+
 //random cocktail: https://www.thecocktaildb.com/api/json/v1/1/random.php
 
 //lookup by id: https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11007
 
 //Add AlcoholicCocktail[] to definitions
 export const getAllCocktails = async (): Promise<AllCocktailsDetails> => {
-  const response = await fetch(`${rootUrl}/filter.php?a=Alcoholic`);
-  const data = await response.json();
-  return data;
+  const response: any = await fetch(`${rootUrl}/filter.php?a=Alcoholi`);
+  // console.log(response, 'response')
+
+  if(response.ok) {
+    return await response.json()
+  } else {
+    throw new Error({ ...response })
+  }
 };
 
 export const getRandomCocktail = async ():Promise<RandomCocktail> => {
