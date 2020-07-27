522a618aa52e5672c1ed4307065e58dd4eda41f3
Alex Sodemann <asodemann18@gmail.com>
2020-07-27 14:46:00 -0600

    Fix failing tests due to name changes

--- src/Header/Header.test.tsx
+++ src/Header/Header.test.tsx
@@ -11,7 +11,8 @@ describe('Header', () => {
         <Header 
           loggedIn={false} 
           setLoggedIn={false}
-          setUsername={''}
+          setUsername={Function}
+				  findResults={Function}
         />
       </MemoryRouter>
     );
@@ -27,7 +28,8 @@ describe('Header', () => {
         <Header 
           loggedIn={true} 
           setLoggedIn={Function}
-          setUsername={''}
+          setUsername={Function}
+				  findResults={Function}
         />
       </MemoryRouter>
     );
@@ -35,21 +37,21 @@ describe('Header', () => {
     const title = getByText('Spirited Quarantini');
     const searchBar = getByPlaceholderText('search cocktails...');
     const aboutBtn = getByText('About');
-    const cocktailsBtn = getByText('Cocktails');
+    const cocktailOfTheDayBtn = getByText('Cocktail of the Day');
     const myCocktailsBtn = getByText('My Cocktails');
     const logoutBtn = getByText('Logout');
 
     expect(title).toBeInTheDocument();
     expect(searchBar).toBeInTheDocument();
     expect(aboutBtn).toBeInTheDocument();
-    expect(cocktailsBtn).toBeInTheDocument();
+    expect(cocktailOfTheDayBtn).toBeInTheDocument();
     expect(myCocktailsBtn).toBeInTheDocument();
     expect(logoutBtn).toBeInTheDocument();
   });
 
 
   // Currently failing because we need to pass in the function that will be later called when clicking the search btn, that function will then be mocked instead of the mock fn I have written below
-  it.skip('User should be able to search cocktails when logged in', () => {
+  it('User should be able to search cocktails when logged in', () => {
     const mockSearch = jest.fn();
 
     const { getByText, getByPlaceholderText } = render(
@@ -57,7 +59,8 @@ describe('Header', () => {
         <Header 
           loggedIn={true} 
           setLoggedIn={Function}
-          setUsername={''}
+          setUsername={Function}
+				  findResults={mockSearch}
           // passed in function that will be used to mock
         />
       </MemoryRouter>
