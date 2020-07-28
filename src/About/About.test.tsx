8434aa7487405e6d478555a46b5de686bc250936
Alex Sodemann <asodemann18@gmail.com>
2020-07-28 15:32:47 -0600

    Add test to About testing file

--- src/About/About.test.tsx
+++ src/About/About.test.tsx
@@ -13,7 +13,8 @@ describe("About", () => {
     );
 
     const aboutInfo = getByText("Spirited Quarantini was created by a team of four women who got tired of having the same boring cocktails every zoom happy hour. We wanted to help others learn how to bartend from the comfort of their own home. There are over 100 recipes to learn how to make delicious, beautiful, and refreshing alcoholic beverages with confidence. Happy mixing!");
-    const aboutImg = getByAltText("")
+    const aboutImg = getByAltText("four people toasting drinks")
     expect(aboutInfo).toBeInTheDocument();
+    expect(aboutImg).toBeInTheDocument();
   })
 })
