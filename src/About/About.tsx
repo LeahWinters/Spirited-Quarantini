2898a56f2304965f4ebd9657791245d4d0b90dd6
Leah Winters <leah.winters24@gmail.com>
2020-07-28 14:30:46 -0600

    :crystal_ball: Add image to assets file

--- src/About/About.tsx
+++ src/About/About.tsx
@@ -8,9 +8,7 @@ const About: React.SFC<AboutProps> = () => {
 
 	return (
 		<section className='about-wrapper'>
-			<div className='cheers-img'>
-				
-			</div>
+			<img className='cheers-img' />
 			<p className='about'>
 			Spirited Quarantini was created by a team of four women who got tired of having the same boring cocktails every zoom happy hour. We wanted to help others learn how to bartend from the comfort of their own home. There are over 100 recipes to learn how to make delicious, beautiful, and refreshing alcoholic beverages with confidence. Happy mixing! 
 			</p>
