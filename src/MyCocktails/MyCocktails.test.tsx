5dfb796d53255f601eea24bbebff20a1e5db3316
Melissa VanKempen <melissa.vankempen@gmail.com>
2020-07-28 10:00:45 -0600

    :bar_chart: completed button displayed test

--- src/MyCocktails/MyCocktails.test.tsx
+++ src/MyCocktails/MyCocktails.test.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import MyCocktails from './MyCocktails';
 import '@testing-library/jest-dom';
-import { render, fireEvent, waitFor } from '@testing-library/react';
+import { render, fireEvent, waitFor, getAllByLabelText } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 
 
 describe ('MyCocktails', () => {
+  it('should have two buttons displayed', () => {
+    const { getByLabelText } = render
+    (<MemoryRouter>
+      <MyCocktails />
+    </MemoryRouter>)
+
+    const favButton = getByLabelText('favorites')
+    const loggedButton = getByLabelText('logged-drinks')
+
+    expect(favButton).toBeInTheDocument()
+    expect(loggedButton).toBeInTheDocument()
+
+  })
+
+  
+
 
 })
