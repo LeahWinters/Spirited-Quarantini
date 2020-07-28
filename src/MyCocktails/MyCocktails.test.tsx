c273099e81199c59cf768db653a2f4a7c58e43f5
Melissa VanKempen <melissa.vankempen@gmail.com>
2020-07-28 09:43:33 -0600

    :tada: initial commit

---
+++ src/MyCocktails/MyCocktails.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import MyCocktails from './MyCocktails';
+import '@testing-library/jest-dom';
+import { render, fireEvent, waitFor } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+
+
+describe ('MyCocktails', () => {
+
+})
